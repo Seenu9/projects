bb9b27a0ef9ab5bb2d8622438bc311abbe533941
srinivas <mr.seenu9392@gmail.com>
2024-06-15 12:06:45 +0530

    pass try

--- app/main.py
+++ app/main.py
@@ -9,9 +9,20 @@ def main():
     connection,address=server_socket.accept()
 
     with connection:
-        while true:
-            request=connection.recv(1024)
-            print(request)
+        while True:
+            data=connection.recv(1024)
+            request=data.decode().split("\r\n")
+            request_line=request[1]
+            if not data:
+                break
+            if request_line=="/":
+                connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            else:
+                connection.sendall(b"HTTP/1.1 404 Not Found \r\n\r\n")
+            connection.close()
+
+
+            
 
 
 if __name__ == "__main__":
