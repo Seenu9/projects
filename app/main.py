195f7e48e75fb383404915c23028fbdbc064608e
srinivas <mr.seenu9392@gmail.com>
2024-06-15 11:57:13 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -5,9 +5,13 @@ import socket
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
     connection,address=server_socket.accept()
-    connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    connection.close()
+
+    with connection:
+        while true:
+            request=connection.recv(1024)
+            print(request)
 
 
 if __name__ == "__main__":
