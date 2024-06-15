d17f49819cf4bfd747672d5aea04d951e30ef3e9
srinivas <mr.seenu9392@gmail.com>
2024-06-15 12:19:55 +0530

    'try'

--- app/main.py
+++ app/main.py
@@ -5,16 +5,14 @@ import socket
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    connection,address=server_socket.accept()
-
-    with connection:
-        while True:
+    while True:
+         connection,address=server_socket.accept()
+         with connection:
             data=connection.recv(1024)
-            request=data.decode().split("\r\n")
-            request_line=request[1]
             if not data:
                 break
+            request=data.decode().split("\r\n")
+            request_line=request[0].split(" ")[1]           
             if request_line=="/":
                 connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
             else:
