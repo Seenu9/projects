7cf62d279f6fd86a0be90cba792111065b7c8fd4
srinivas <mr.seenu9392@gmail.com>
2024-06-18 15:24:34 +0530

    fafga

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ def main():
                     e_str=path[(len("/echo/")):]
                     e_str_comprssed=str(compress(e_str)).split("'")[1]                    
                     if "gzip" in encoding:
-                        response=f"HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {len(e_str_comprssed)}\r\n\r\n{hexdeci}".encode()                
+                        response=f"HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {len(e_str_comprssed)}\r\n\r\n{e_str_comprssed}".encode()                
                     else:
                         response=f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(e_str)}\r\n\r\n{e_str}".encode()
                 elif path.startswith("/user-agent"):
