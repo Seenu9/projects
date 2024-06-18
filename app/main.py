6dce734fbffdb08fb1c508aa082e275f546117d5
srinivas <mr.seenu9392@gmail.com>
2024-06-18 09:56:37 +0530

    finally

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 
 import socket
 import sys
+import binascii
 import gzip
 
 def compress(input_string):
@@ -34,8 +35,9 @@ def main():
                 elif path.startswith("/echo/"):
                     e_str=path[(len("/echo/")):]
                     e_str_comprssed=compress(e_str)
+                    hexdeci=binascii.hexlify(e_str_comprssed).decode()
                     if "gzip" in encoding:
-                        response=f"HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {len(e_str_comprssed)}\r\n\r\n{e_str_comprssed}".encode()                
+                        response=f"HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {len(e_str_comprssed)}\r\n\r\n{hexdeci}".encode()                
                     else:
                         response=f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(e_str)}\r\n\r\n{e_str}".encode()
                 elif path.startswith("/user-agent"):
